16f423e1c86a569d72359302d33c676c14e17e67
Aman <ak193566@gmail.com>
2023-12-05 00:34:39 +0530

    update images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import Cards from './Cards'
 
 function Home() {
+    // const dets = [
+    //     {img:"https://cdn.weartesters.com/wp-content/uploads/2018/10/nike-lebron-soldier-12-team.jpg", text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Black)",},
+    //     {img:"https://cdn.fashdirect.com/webadmin/items/website/3827034/image_2_404460025500_1.jpg", text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Blue)",},
+    //     {img:"https://cdn.weartesters.com/wp-content/uploads/2018/10/nike-lebron-soldier-12-team-colorways-red.jpg", text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Red)",},
+    //     {img:"https://cdn.weartesters.com/wp-content/uploads/2018/10/nike-lebron-soldier-12-team.jpg", text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Black)",},
+    //     {img:"https://cdn.fashdirect.com/webadmin/items/website/3827034/image_2_404460025500_1.jpg", text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Blue)",},
+    //     {img:"https://cdn.weartesters.com/wp-content/uploads/2018/10/nike-lebron-soldier-12-team-colorways-red.jpg", text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Red)",},
+    // ]
     const dets = [
-        {img:"https://cdn.weartesters.com/wp-content/uploads/2018/10/nike-lebron-soldier-12-team.jpg", text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Black)",},
+        {img:"https://static.nike.com/a/images/t_default/e8cbcfc4-ce2e-42d7-a7a2-86bf3e1dd506/lebron-witness-7-basketball-shoes-lLTcQM.png", text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Black)",},
         {img:"https://cdn.fashdirect.com/webadmin/items/website/3827034/image_2_404460025500_1.jpg", text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Blue)",},
+        {img:"https://static.nike.com/a/images/t_default/b3506e37-a78a-4283-a475-00c0bfc9c1a6/lebron-witness-7-older-basketball-shoes-bV8fWg.png",text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Blue)",},
         {img:"https://cdn.weartesters.com/wp-content/uploads/2018/10/nike-lebron-soldier-12-team-colorways-red.jpg", text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Red)",},
+        {img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPGC-CKx2W2ctHo2k9pcNbMXM3qFe_d6cW6Q&usqp=CAU", text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Red)",},
         {img:"https://cdn.weartesters.com/wp-content/uploads/2018/10/nike-lebron-soldier-12-team.jpg", text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Black)",},
-        {img:"https://cdn.fashdirect.com/webadmin/items/website/3827034/image_2_404460025500_1.jpg", text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Blue)",},
-        {img:"https://cdn.weartesters.com/wp-content/uploads/2018/10/nike-lebron-soldier-12-team-colorways-red.jpg", text1:"Men's Basketball Shoe", text2:"LeBron Solider 12 (Red)",},
     ]
   return (
     <div className='relative contantHeight w-full md:flex ps-[20px] md:ps-[50px] lg:ps-[120px]'>
@@ -16,7 +24,7 @@ function Home() {
             <h5 className='text-xs md:text-sm font-semibold text-gray-500 mt-8 md:mt-10'>Explore the Limited Edition</h5>
             <h1 className=' text-[20px] md:text-[40px] lg:text-[60px] uppercase leading-none lg:leading-[3.5rem] my-2 md:my-4 font-semibold'>Nike Limited Edition</h1>
             <p className='text-xs md:text-sm w-[70%] leading-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem consequuntur nesciunt minus sit in maxime fuga, aperiam sequi odio esse culpa laudantium itaque facilis molestias eligendi eaque? Id, quaerat doloribus!</p>
-            <button className='my-2 md:my-6  bg-black text-white text-sm md:text-[16px] uppercase py-2 px-6 rounded-3xl rounded-tl-none'>Shop Now</button>
+            <button className='my-2 md:my-6  bg-black text-white text-sm md:text-[16px] uppercase py-2 px-6 rounded-3xl rounded-tl-none cursor-pointer'>Shop Now</button>
         </div>
         <div id='right' className='relative h-[65%] md:h-[70%] lg:h-[95%] w-full md:w-[60%] whitespace-nowrap overflow-x-auto overflow-y-hidden pb-2 mt-4 md:mt-6 lg:mt-0'>
             {dets.map((data)=>(
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,22 +6,22 @@ function NavBar() {
   return (
     <div className='relative md:h-[110px] w-full flex justify-between items-center px-[20px] md:px-[80px] mt-4 md:mt-0'>
         <div className='flex items-center'>
-            <img src={logo} alt='nikeLogo' className='h-[35px] object-cover me-[60px] lg:me-[170px]'/>
+            <img src={logo} alt='nikeLogo' className='h-[45px] md:h-[70px] object-cover me-[60px] lg:me-[170px]'/>
             <ul id='nav' className='flex justify-evenly w-[40vw] font-semibold'>
-                <li className='uppercase me-[60px] text-[14px] font-semibold'>
+                <li className='uppercase me-[60px] text-[14px] font-semibold cursor-pointer'>
                     Home
                     <div className='h-[3px] bg-black w-[30px] rounded-lg'></div>    
                 </li>
-                <li className='uppercase me-[60px] text-[14px] font-semibold'>Men</li>
-                <li className='uppercase me-[60px] text-[14px] font-semibold'>Women</li>
-                <li className='uppercase me-[60px] text-[14px] font-semibold'>Collection</li>
-                <li className='uppercase me-[60px] text-[14px] font-semibold'>Contact</li>
+                <li className='uppercase me-[60px] text-[14px] font-semibold cursor-pointer'>Men</li>
+                <li className='uppercase me-[60px] text-[14px] font-semibold cursor-pointer'>Women</li>
+                <li className='uppercase me-[60px] text-[14px] font-semibold cursor-pointer'>Collection</li>
+                <li className='uppercase me-[60px] text-[14px] font-semibold cursor-pointer'>Contact</li>
             </ul>
         </div>
         <div className=''>
-            <i class="ri-search-line me-[30px] lg:me-[60px] text-lg md:text-[20px] font-normal"></i>
-            <i class="ri-shopping-cart-line me-[30px] lg:me-[60px] text-lg md:text-[20px] font-normal"> 0</i>
-            <i class="ri-menu-3-line me-[30px] lg:me-[60px] text-lg md:text-[20px] font-normal"></i>
+            <i className="ri-search-line me-[30px] lg:me-[60px] text-lg md:text-[20px] font-normal cursor-pointer"></i>
+            <i className="ri-shopping-cart-line me-[30px] lg:me-[60px] text-lg md:text-[20px] font-normal cursor-pointer"> 0</i>
+            <i className="ri-menu-3-line me-[30px] lg:me-[60px] text-lg md:text-[20px] font-normal cursor-pointer"></i>
         </div>
     </div>
   )
