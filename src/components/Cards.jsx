eca391a62dc56e98de9b4e0268ed07e3d2fa7f70
Aman <ak193566@gmail.com>
2023-12-05 00:22:46 +0530

    Update Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,7 +7,7 @@ function Cards({
 }) {
   return (
     <>
-    <div className='relative w-[250px] md:w-[300px] h-full bg-white rounded-lg inline-block me-4 overflow-hidden' style={{boxShadow:"1px 1px 4px black"}}>
+    <div className='relative w-[250px] md:w-[300px] h-full bg-white rounded-lg inline-block me-4 overflow-hidden' style={{boxShadow:"2px 2px 10px rgba(0,0,0,0.3)"}}>
         <img src={img} alt="" className='absolute h-[50%] w-full object-cover top-[7%]'/>
         <h4 className='absolute bottom-[10%] right-0 bg-black text-white py-[12px] px-[24px] rounded-lg rounded-br-none font-semibold text-sm'>{text1}</h4>
         <h5 className='absolute bottom-0 right-0 bg-[#D0D4D5] py-[12px] px-[30px] rounded-lg rounded-tr-none text-sm'>{text2}</h5>
